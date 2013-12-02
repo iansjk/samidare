ce2d1ddc9a2c17b6f3bf7bcf8231f8f949ced01b
Ian Kim <iansjk@gmail.com>
2013-12-02 18:56:31 -0500

    Added routing+viewfuncs for artist and song detail views

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import patterns, url
 from lyrics import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    # ../lyrics/
+    url(r'^$', views.index, name='index'),
+    # ../lyrics/<artist-slug>
+    url(r'^(?P<artist_slug>[a-z0-9\-]+)$', views.artist, name="artist"),
+    # ../lyrics/<artist-slug>/<song-slug>
+    url(r'^(?P<artist_slug>[a-z0-9\-]+)/(?P<song_slug>[a-z0-9\-]+)$',
+        views.song, name="song"),
 )
--- lyrics/views.py
+++ lyrics/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse('Nothing here but us trees!')
+
+def artist(response, artist_slug):
+    return HttpResponse('Artist slug: "{0}"'.format(artist_slug))
+
+def song(response, artist_slug, song_slug):
+    return HttpResponse('Artist slug: "{0}", Song slug: "{1}"'.format(
+        artist_slug, song_slug))
