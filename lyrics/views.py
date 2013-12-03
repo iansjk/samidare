f31922df3aec6831b9bfb4976a256abff8f350b9
Ian Kim <iansjk@gmail.com>
2013-12-02 19:06:24 -0500

    Updated Artist & Song views to use data in DB

--- lyrics/views.py
+++ lyrics/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from lyrics.models import Song, Artist, Album
 
 def index(response):
     return HttpResponse('Nothing here but us trees!')
 
 def artist(response, artist_slug):
-    return HttpResponse('Artist slug: "{0}"'.format(artist_slug))
+    artist = get_object_or_404(Artist, slug=artist_slug)
+    return HttpResponse('Found artist: {0}'.format(artist))
 
 def song(response, artist_slug, song_slug):
-    return HttpResponse('Artist slug: "{0}", Song slug: "{1}"'.format(
-        artist_slug, song_slug))
+    song = get_object_or_404(Song, artist__slug=artist_slug, slug=song_slug)
+    return HttpResponse('Found song: {0}'.format(song))
