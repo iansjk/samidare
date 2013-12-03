ff039cb7efae5785b8bdf9234364c3cba52c4357
Ian Kim <iansjk@gmail.com>
2013-12-03 18:22:22 -0500

    Reformatted and added description to SongAdmin lyricsheet fieldset
    The description lies! Leading/trailing extraneous whitespace isn't stripped
    yet. That'll come a bit later. (It's not as simple as calling strip() since it
    needs to be removed between '\n\n'.)
    
    Also, '\r\n' is not replaced by '\n' or some canonical newline representation.
    (But it should be.)

--- lyrics/admin.py
+++ lyrics/admin.py
@@ -3,12 +3,18 @@ from lyrics.models import Song, Artist, Album
 
 class SongAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('General information',
-                {'fields': ['title_en', 'title_orig', 'artist', 'album']}
-        ),
-        ('Lyricsheet',
-                {'fields': ['original', 'romanized', 'translated']}
-        ),
+        ('General information', {
+            'fields': ['title_en', 'title_orig', 'artist', 'album'],
+        }),
+        ('Lyricsheet', {
+            'description':
+                ('Enter lyricsheet information here. Full HTML is allowed. A '
+                 'single newline will be replaced with &lt;br&gt;, and two '
+                 'linebreaks will be interpreted as the start of a new '
+                 '&lt;p&gt; element. Leading or trailing whitespace will be '
+                 'removed on each line.'),
+            'fields': ['original', 'romanized', 'translated'],
+        }),
     ]
     list_display = (
         '__unicode__',  # title_orig if it exists, title_en otherwise
