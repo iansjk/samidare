e1a9a8214cced9fad61ae7c5d2e3edbf508a841f
Ian Kim <iansjk@gmail.com>
2013-12-02 19:24:20 -0500

    Fixed up views.py naming/imports

--- lyrics/views.py
+++ lyrics/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from lyrics.models import Song, Artist, Album
+from lyrics.models import Song, Artist
 
-def index(response):
+def index(request):
     return HttpResponse('Nothing here but us trees!')
 
-def artist(response, artist_slug):
+def artist(request, artist_slug):
     artist = get_object_or_404(Artist, slug=artist_slug)
     return HttpResponse('Found artist: {0}'.format(artist))
 
-def song(response, artist_slug, song_slug):
+def song(request, artist_slug, song_slug):
     song = get_object_or_404(Song, artist__slug=artist_slug, slug=song_slug)
     return HttpResponse('Found song: {0}'.format(song))
